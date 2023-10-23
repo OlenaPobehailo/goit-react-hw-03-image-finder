73dbf45f766651f56fa2d28fd9e562e112afe0ba
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-10-23 21:17:45 +0300

    creates project structure

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,13 @@
+import ImageGalleryItem from 'components/ImageGalleryItem';
+import React from 'react';
+
+const ImageGallery = () => {
+  return (
+    <div>
+      ImageGallery
+      <ImageGalleryItem />
+    </div>
+  );
+};
+
+export default ImageGallery;
---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ImageGalleryItem = () => {
+  return <div>ImageGalleryItem</div>;
+};
+
+export default ImageGalleryItem;
